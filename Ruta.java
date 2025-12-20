e1f2d221423f89434d050a1b2a3866574ee0ff20
Javier4E <javier.echeverria.soto@alumnos.uta.cl>
2025-12-20 13:05:40 -0300

    Update Ruta.java

--- Ruta.java
+++ Ruta.java
@@ -3,46 +3,42 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyectoaed;
+import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 /**
  *
  * @author PC GAMER
  */
 public class Ruta {
-    List<Arco> arcos;
-    int costoTotal;
-    
-    public Ruta(Paradero paraderoOrigen, Paradero paraderoLlegada){
-        this.arcos = calcularRuta(paraderoOrigen, paraderoLlegada);
-        this.costoTotal = 0;
-        
-        for (Arco a: arcos) {
-            this.costoTotal += a.getPeso();
-        }   
+
+    private  List<Arco> arcos;
+    private int costoTotal;
+
+    // La ruta YA viene calculada
+    public Ruta(List<Arco> arcos) {
+        this.arcos = new ArrayList<>(arcos); // copia defensiva
+
+        int costo = 0;
+        for (Arco a : arcos) {
+            costo += a.getPeso();
+        }
+        this.costoTotal = costo;
     }
-    
-    public List<Arco> getNodos() {
-        return arcos;
+
+    public List<Arco> getTramos() {
+        return Collections.unmodifiableList(arcos);
     }
-    
+
     public Arco getArcoIndex(int i) {
         return arcos.get(i);
     }
-    
-    public int getCostoTotal(){
+
+    public double getCostoTotal() {
         return costoTotal;
     }
-    
-/*    
-    public String toString() {
-        String st;
-        for (int i = 0; i < arcos.size() - 1; i++) {
-            Paradero paradero1 = arcos.get(i);
-            Paradero paradero2 = arcos.get(i+1);
-            
-            
-        }
-        
-        return st;
+
+    public boolean estaVacia() {
+        return arcos.isEmpty();
     }
 }
