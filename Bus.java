2d1bc2bf0d125ce28c2d9c7bbfa4fac43ebce254
Javier4E <javier.echeverria.soto@alumnos.uta.cl>
2025-12-20 13:05:03 -0300

    Update Bus.java

--- Bus.java
+++ Bus.java
@@ -4,103 +4,119 @@
  */
 package proyectoaed;
 
+import java.util.List;
 /**
  *
  * @author PC GAMER
  */
 public class Bus {
-    int id;
-    
-    Paradero paraderoOrigen;
-    Paradero paraderoLlegada;
-    Paradero paraderoActual;
-    int indiceActual = 0;
-    
-    Arco arcoActual;
-    Ruta rutaIda;
-    Ruta rutaVuelta;
-    Ruta rutaActual;
-    
-    int tiempoAcumuladoMin = 0;
-    int tiempoRestanteArco = 0;
-    
-    int tiempoSobrante;
-    
-    public Bus(int id, Paradero paraderoOrigen, Paradero paraderoLlegada){
+
+    private int id;
+
+    private Paradero paraderoOrigen;
+    private Paradero paraderoLlegada;
+    private Paradero paraderoActual;
+
+    private Ruta rutaIda;
+    private Ruta rutaVuelta;
+    private Ruta rutaActual;
+
+    private Arco arcoActual;
+    private int indiceActual;
+
+    private int tiempoAcumuladoMin;
+    private int tiempoRestanteArco;
+
+    public Bus(int id, Paradero paraderoOrigen, Paradero paraderoLlegada, List<Arco> rutaIda, List<Arco> rutaVuelta) {
         this.id = id;
         this.paraderoOrigen = paraderoOrigen;
         this.paraderoLlegada = paraderoLlegada;
         this.paraderoActual = paraderoOrigen;
-        
-         //NECESITO ESTAs FUNCIONES
-        this.rutaIda = new Ruta(paraderoOrigen, paraderoLlegada);
-        this.rutaVuelta = new Ruta(paraderoLlegada, paraderoOrigen);
+
+        // Idealmente estas rutas las entrega el Grafo
+        this.rutaIda = new Ruta(rutaIda);
+        this.rutaVuelta = new Ruta(rutaVuelta);
+
+        this.arcoActual = null;
+        this.indiceActual = 0;
+        this.tiempoAcumuladoMin = 0;
+        this.tiempoRestanteArco = 0;
     }
-    
+
     public boolean iniciarRuta() {
+
         if (paraderoActual.equals(paraderoOrigen)) {
             rutaActual = rutaIda;
-        }
-        
-        else if (paraderoActual.equals(paraderoLlegada)) {
+        } else if (paraderoActual.equals(paraderoLlegada)) {
             rutaActual = rutaVuelta;
-        }
-        
-        else {
+        } else {
             return false;
         }
-        
+
+        if (rutaActual.getTramos().isEmpty()) {
+            return false;
+        }
+
         tiempoAcumuladoMin = 0;
-        
         indiceActual = 0;
-        arcoActual = rutaActual.getNodos().get(0);
-        
+
+        arcoActual = rutaActual.getTramos().get(0);
         tiempoRestanteArco = arcoActual.getPeso();
+
         return true;
     }
-    
+
+    /**
+     * @return true si llegó al final de la ruta, false si sigue en movimiento
+     */
     public boolean avanzar10Min() {
+
         tiempoAcumuladoMin += 10;
-        int tiempoTotal = tiempoSobrante + 10;
-        
-        if (tiempoRestanteArco > tiempoTotal) { //Bus no llega a siguente paradero
-            
-            tiempoRestanteArco -= tiempoTotal;
-            tiempoSobrante = 0;
-            
-            paraderoActual = null;
-            arcoActual = rutaActual.getArcoIndex(indiceActual);
-            return false;
-        }
-        
-        else if (tiempoRestanteArco < tiempoTotal) { //Bus se pasa del siguente paradero
-            
-            indiceActual += 1;
-            if (indiceActual == rutaActual.getNodos().size() - 1) {   //Llega a ultimo paradero de la ruta
-                paraderoActual = rutaActual.getArcoIndex(indiceActual).getParadero(0);
-                arcoActual = null;
-                tiempoSobrante =  tiempoTotal - tiempoRestanteArco;
-                tiempoRestanteArco = 0;
-                return true;
+        int tiempoDisp = 10;
+
+        while (tiempoDisp > 0 && arcoActual != null) {
+
+            if (tiempoRestanteArco > tiempoDisp) {
+                tiempoRestanteArco -= tiempoDisp;
+                tiempoDisp = 0;
+            } else {
+                tiempoDisp -= tiempoRestanteArco;
+
+                paraderoActual = arcoActual.getDestino();
+                indiceActual++;
+
+                if (indiceActual >= rutaActual.getTramos().size()) {
+                    arcoActual = null;
+                    tiempoRestanteArco = 0;
+                    return true;
+                }
+
+                arcoActual = rutaActual.getTramos().get(indiceActual);
+                tiempoRestanteArco = arcoActual.getPeso();
             }
-            else{
-                paraderoActual = null;
-                arcoActual = rutaActual.getArcoIndex(indiceActual);
-                
-                tiempoSobrante = tiempoTotal - tiempoRestanteArco;
-                tiempoRestanteArco = arcoActual.getPeso() - 
-            }
-            paraderoActual = null;
-            arcoActual = rutaActual.getArcoIndex(indiceActual);
-            
-            tiempoSobrante = tiempoTotal - tiempoRestanteArco;
-            tiempoRestanteArco = arcoActual.getPeso();
-            
         }
-        else { //Bus llega justamente al paradero
-            indiceActual += 1;
-            paraderoActual = rutaActual.getArcoIndex(indiceActual).getParadero(0); //Nececito getParadero
-            
-        }
+
+        return false;
+    }
+
+    // Getters útiles
+    public int getId() {
+        return id;
+    }
+
+    public Paradero getParaderoActual() {
+        return paraderoActual;
+    }
+
+    public Arco getArcoActual() {
+        return arcoActual;
+    }
+
+    public int getTiempoAcumuladoMin() {
+        return tiempoAcumuladoMin;
+    }
+
+    public boolean isEnMovimiento() {
+        return arcoActual != null;
     }
 }
